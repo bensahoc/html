7263d0a10eadc5b02514ae3962ba7e1ea76fa894
benaadi <y.bensaadi@gmail.com>
2017-03-03 12:26:53 +0100

    nouveau commit

--- cv.js
+++ cv.js
@@ -4,4 +4,21 @@
   $(id_div).toggle();
   }
 
-
+function validation() {	  
+  var contenu_email = $('#courriel').val();  
+  var contenu_num = $('#telephone').val(); 
+     valider_telephone(contenu_num);
+     valider_email(contenu_email);
+	  }
+	   
+	  function valider_email(email){
+		  if(email.incules('@')== false)
+		  alert("email non valide");
+	  }
+	  function valider_telephone(num){
+      var valide= /^0[1-6]\d{8}$/;
+		  if(valide.test(num)==false)
+		  window.alert("votre telephone doit contenir des numero");
+		  if (num.length != 10 )
+		  alert("téléphone non valide");
+	  }
